1813202b60f53afc238c1d6819e7158cb9b9ce53
beggarking1002 <drake1002@naver.com>
2024-08-01 04:20:24 +0900

    Connector
    Connector

--- ServerCore/Session.cs
+++ ServerCore/Session.cs
@@ -7,7 +7,7 @@ using System.Threading;
 
 namespace ServerCore
 {
-    internal abstract class Session
+    public abstract class Session
     {
         Socket _socket;
         int _disconnected = 0;
