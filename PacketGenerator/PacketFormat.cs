255340abd643878dbad090c1471bcf2a7ccb0a39
beggarking1002 <drake1002@naver.com>
2024-08-09 02:29:53 +0900

    Packet Generator#2
    Packet Generator#2

--- PacketGenerator/PacketFormat.cs
+++ PacketGenerator/PacketFormat.cs
@@ -14,7 +14,7 @@ namespace PacketGenerator
 
 @"
 
-class {0}}
+class {0}
 {{
     {1}
 
@@ -56,7 +56,34 @@ class {0}}
         // {0} 변수 형식
         // {1} 변수 이름
         public static string memberFormat =
-@"public {0} {1}";
+@"public {0} {1};";
+
+        // {0} 리스트 이름 [대문자]
+        // {1} 리스트 이름 [소문자]
+        // {2} 멤버 변수들
+        // {3} 멤버 변수 Read
+        // {4} 멤버 변수 Write
+        public static string memberListFormat =
+@"
+ public struct {0}
+{{
+    {2}
+
+    public void Read(ReadOnlySpan<byte> s, ref ushort count)
+    {{
+        {3}
+    }}
+    public bool Write(Span<byte> s, ref ushort count)
+    {{
+        bool success = true;
+        {4}
+        return success;
+    }}
+
+    
+}}
+public List<{0}> {1}s = new List<{0}>();";
+
 
         // {0} 변수 이름
         // {1} TO~ 변수 형식
@@ -73,6 +100,23 @@ count += sizeof(ushort);
 this.{0} = Encoding.Unicode.GetString(s.Slice(count, {0}Len));
 count += {0}Len;";
 
+
+        // {0} 리스트 이름 [대문자]
+        // {1} 리스트 이름 [소문자]
+
+        public static string readListFormat =
+@"this.{1}s.Clear();
+ushort {1}Len = BitConverter.ToUInt16(s.Slice(count, s.Length - count));
+count += sizeof(ushort);
+            
+for(int i=0; i<{1}Len; i++)
+{{
+    {0} {1} = new {0}();
+    {1}.Read(s, ref count);
+    {1}s.Add({1});
+}}";
+
+
         // {0} 변수 이름
         // {1} 변수 형식
         public static string writeFormat =
@@ -84,8 +128,22 @@ count += sizeof({1});";
 @"ushort {0}Len = (ushort)Encoding.Unicode.GetBytes(this.{0}, 0, this.{0}.Length, segment.Array, segment.Offset + count + sizeof(ushort));
 success &= BitConverter.TryWriteBytes(s.Slice(count, s.Length - count), {0}Len);
 count += sizeof(ushort);
-count += {0}Len;
+count += {0}Len;";
+
+
+        // {0} 리스트 이름 [대문자]
+        // {1} 리스트 이름 [소문자]
+
+        public static string writeListFormat =
+@"success &= BitConverter.TryWriteBytes(s.Slice(count, s.Length - count), (ushort)this.{1}s.Count);
+count += sizeof(ushort);
+foreach({0} {1} in this.{1}s)
+{{
+    success &= {1}.Write(s, ref count);
+}}
+
 ";
+
     }
 
 }
